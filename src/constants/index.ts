a7756cb45487d75e95b5d51d97aa29a69898bfba
Abhisheksaraswat21 <abhisaraswattgx@gmail.com>
2025-11-29 09:15:00 +0530

    Add default constants for file validation and upload limits

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+export const DEFAULT_MAX_FILES = 10;
+export const DEFAULT_ACCEPTED_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/gif",
+  "image/webp",
+];
